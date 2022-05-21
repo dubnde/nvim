5813a4fcb1136265baeb8c77b1fe415c7f12ccab
Dubai Chingalande <dchingal@qti.qualcomm.com>
2022-05-21 12:38:19 +0100

    Initial Vanilla Setup

---
+++ lua/plugins.lua
@@ -0,0 +1,131 @@
+local M = {}
+
+function M.setup()
+  -- Indicate first time installation
+  local packer_bootstrap = false
+
+  -- packer.nvim configuration
+  local conf = {
+    profile = {
+      enable = true,
+      threshold = 0, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+    },
+
+    display = {
+      open_fn = function()
+        return require("packer.util").float { border = "rounded" }
+      end,
+    },
+  }
+
+  -- Check if packer.nvim is installed
+  -- Run PackerCompile if there are changes in this file
+  local function packer_init()
+    local fn = vim.fn
+    local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+      packer_bootstrap = fn.system {
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+      }
+      vim.cmd [[packadd packer.nvim]]
+    end
+    vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
+  end
+
+  -- Plugins
+  local function plugins(use)
+    use { "wbthomason/packer.nvim" }
+
+    -- Performance
+    use { "lewis6991/impatient.nvim" }
+
+    -- Load only when require
+    use { "nvim-lua/plenary.nvim", module = "plenary" }
+
+    -- Notification
+    use {
+      "rcarriga/nvim-notify",
+      event = "VimEnter",
+      config = function()
+        vim.notify = require "notify"
+      end,
+    }
+
+    use {
+      "sainnhe/everforest",
+      config = function()
+        vim.g.everforest_better_performance = 1
+        vim.cmd "colorscheme everforest"
+      end,
+      disable = false,
+    }
+
+    -- Git
+    use {
+      "TimUntersberger/neogit",
+      cmd = "Neogit",
+      config = function()
+        require("config.neogit").setup()
+      end,
+    }
+
+    use {
+      "lewis6991/gitsigns.nvim",
+      event = "BufReadPre",
+      wants = "plenary.nvim",
+      requires = { "nvim-lua/plenary.nvim" },
+      config = function()
+        require("config.gitsigns").setup()
+      end,
+    }
+
+    -- IndentLine
+    use {
+      "lukas-reineke/indent-blankline.nvim",
+      event = "BufReadPre",
+      config = function()
+        require("config.indentblankline").setup()
+      end,
+    }
+
+    -- Better surround
+    use { "tpope/vim-surround", event = "InsertEnter" }
+
+    -- Buffer
+    use { "kazhala/close-buffers.nvim", cmd = { "BDelete", "BWipeout" } }
+
+    -- Easy motion
+    use {
+      "phaazon/hop.nvim",
+      cmd = { "HopWord", "HopChar1" },
+      config = function()
+        require("hop").setup {}
+      end,
+      disable = true,
+    }
+
+    -- Bootstrap Neovim
+    if packer_bootstrap then
+      print "Restart Neovim required after installation!"
+      require("packer").sync()
+    end
+  end
+
+  -- Init and start packer
+  packer_init()
+  local packer = require "packer"
+
+  -- Performance
+  pcall(require, "impatient")
+  -- pcall(require, "packer_compiled")
+
+  packer.init(conf)
+  packer.startup(plugins)
+end
+
+return M
